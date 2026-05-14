73f28e3f209a0f7246e5c1002fe8b83e38c11bed
Varisa Chandrabose <varisibunny@gmail.com>
2026-05-14 22:59:22 +0530

    Update DatabaseConnection.java

--- src/main/java/javaclass/DatabaseConnection.java
+++ src/main/java/javaclass/DatabaseConnection.java
@@ -5,12 +5,24 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
-    private static final String URL = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
-    private static final String USER = "Fd3c3RUAfdrJJQk.root";
-    private static final String PASSWORD = "nwyLNH7KFkZfkTs4";
+    private static final String URL =
+    "jdbc:mysql://gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/onlinebookstore?sslMode=VERIFY_IDENTITY";
 
-    public static Connection getConnection() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private static final String USER =
+    "Fd3c3RUAfdrJJQk.root";
+
+    private static final String PASSWORD =
+    "nwyLNH7KFkZfkTs4";
+
+    public static Connection getConnection()
+            throws Exception {
+
+        Class.forName(
+        "com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+        URL,
+        USER,
+        PASSWORD);
     }
 }
